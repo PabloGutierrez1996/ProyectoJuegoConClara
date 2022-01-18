173c4cc33bc7a1d18b6ab0739d54552e0c6e7414
PabloGutierrez1996 <pablomation@gmail.com>
2022-01-18 15:00:24 -0600

    definicione basicas 1

--- Juego.py
+++ Juego.py
@@ -2,7 +2,27 @@ import pygame
 import random
 import sys
 
-pygame.init()
-
+#Definiciones
 WIDTH = 800
 HEIGHT = 600
+color_verde=(0,0,255)
+color_negro=(0,0,0)
+color_azul = (0,0,255)
+
+#Personaje del Jugador
+
+
+
+
+
+#Enemigos a evitar que hay que evitar
+
+
+
+
+
+# Aqui empieza la Consola donde se proyecta el juego. 
+consola = pygame.display.set_mode((WIDTH,HEIGHT))
+
+game_over = False
+clock = pygame.time.Clock()
