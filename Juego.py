c50f6620b4292e65dca1aefc06812a4b1e482f5e
claradof95 <ccoradofalla@gmail.com>
2022-01-18 19:39:12 -0600

    Update Juego.py

--- Juego.py
+++ Juego.py
@@ -1,16 +1,18 @@
 
-import pygame
+from msilib.schema import Font
+import pygame #acceder al comando de pygame
 import random
 import sys
 
 
-pygame.init()
+pygame.init() #inicializa los módulos de pygame
 #Definiciones
 ANCHO = 800
 ALTO = 600
-color_verde=(0,0,255)
+color_rojo=(255,0,0)
 color_negro=(0,0,0)
 color_azul = (0,255,0)
+color_texto = (130,130,130)
 
 #Personaje del Jugador
 pos_juga = [400,400] #lista de valores de x,y del jugador. la x se representa por [0] y la "y" por [1]
@@ -26,10 +28,10 @@ pos_enemigo = [0,300] #para que salgan desde arriba y centrados de la pantalla
 
 # Aqui empieza la Consola donde se proyecta el juego. 
 consola = pygame.display.set_mode((ANCHO,ALTO))
-
-game_over = False
+pygame.display.set_caption("Fin del juego")
 clock = pygame.time.Clock()
-#quiero ver si funciona bien esto
+fuente = pygame.font.Font(None, 36)
+game_over = False
 #Funciones de colicion con el enemigo
 def detectar_colision(pos_juga,pos_enemigo):
 	jx = pos_juga[0]
@@ -47,7 +49,7 @@ def detectar_colision(pos_juga,pos_enemigo):
 while not game_over:
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
-			sys.exit()
+			sys.exit
 
 		if event.type == pygame.KEYDOWN:
 			x = pos_juga[0]
@@ -68,13 +70,28 @@ while not game_over:
 	if detectar_colision(pos_juga,pos_enemigo):
 		game_over = True
 
+#Dibujamos el rectangulo con texto de game over
+		if game_over:
+			texto = fuente.render("FIN DEL JUEGO", True, color_texto)
+			texto_rect = texto.get_rect()
+			texto_x = consola.get_width()/2 - texto_rect.width / 2
+			texto_y = consola.get_height()/2 - texto_rect.height / 2
+			consola.blit(texto, [texto_x, texto_y])
+		else:
+			texto = fuente.render("Haz click para terminar el juego", True, color_texto)
+			texto_rect = texto.get_rect()
+			texto_x = consola.get_width() / 2 - texto_rect.width / 2
+			texto_y = consola.get_height() / 2 - texto_rect.height / 2
+			consola.blit(texto, [texto_x, texto_y])
+
 #Dibujar enemigo
 	pygame.draw.rect(consola, color_azul,
 			(pos_enemigo[0],pos_enemigo[1],
 			tam_enemigo, tam_enemigo))
 	#Dibujar jugador
-	pygame.draw.rect(consola, color_verde,
+	pygame.draw.rect(consola, color_rojo,
 			(pos_juga[0],pos_juga[1],
 			tam_juga,tam_juga))
-	clock.tick(70)
-	pygame.display.update()
+	clock.tick(50)
+	pygame.display.flip()
+pygame.quit()
